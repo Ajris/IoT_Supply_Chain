d6fb08cd3cffc0a4d6150f93a1e5d9a6bb92ef82
kshalot <kszuster@protonmail.com>
2020-04-26 23:55:58 +0200

    add deployer

--- brownie/scripts/deploy.py
+++ brownie/scripts/deploy.py
@@ -2,6 +2,6 @@ from brownie import HelloWorld, accounts
 
 
 def main():
-    accounts.load(accounts.load()[0])
-    HelloWorld.deploy({'from': accounts[0]})
+    account = accounts.load('deployer')
+    HelloWorld.deploy({'from': account})
 
