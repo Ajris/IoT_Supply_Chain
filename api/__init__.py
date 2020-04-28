4ae33a22d25451e54840f2e8e34d3927f64e5e2c
kshalot <kszuster@protonmail.com>
2020-04-29 00:07:21 +0200

    add web3py

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from web3 import Web3
 
 app = Flask(__name__)
+w3 = Web3(Web3.HTTPProvider("http://localhost:8545"))
+w3.eth.defaultAccount = w3.eth.accounts[0]
 
 from . import views
 
