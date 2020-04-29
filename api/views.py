90d034e4a27e08ffaf5be2ab20b0cf940d4a427b
Karol Szuster <kszuster@protonmail.com>
2020-04-29 23:40:53 +0200

    change return value

--- api/views.py
+++ api/views.py
@@ -14,5 +14,5 @@ def index():
 
     tx_hash = contract.functions.balanceOf('0xA06BC4aa79553F765b6a3548ee4f2d4a3C76715F').transact()
     receipt = w3.eth.waitForTransactionReceipt(tx_hash)
-    return receipt['status']
+    return {'status': receipt['status']}
 
