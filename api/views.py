4d63e6f277bf5dd60ac3516118da7bbfc6f89c3d
Karol Szuster <kszuster@protonmail.com>
2020-04-30 19:33:15 +0200

    major refactor

--- api/views.py
+++ api/views.py
@@ -2,7 +2,8 @@ import json
 
 from . import app, w3
 
-ADDRESS = '0xDc67312a5302D0e113A8c27220D4F3281AfeB9d2'
+CONTRACT_ADDRESS = '0xDc67312a5302D0e113A8c27220D4F3281AfeB9d2'
+WALLET_ADDRESS = '0xA06BC4aa79553F765b6a3548ee4f2d4a3C76715F'
 
 
 @app.route('/')
@@ -12,7 +13,7 @@ def index():
     abi = source['abi']
     contract = w3.eth.contract(address=ADDRESS, abi=abi)
 
-    tx_hash = contract.functions.balanceOf('0xA06BC4aa79553F765b6a3548ee4f2d4a3C76715F').transact()
+    tx_hash = contract.functions.balanceOf(WALLET_ADDRESS).transact()
     receipt = w3.eth.waitForTransactionReceipt(tx_hash)
     return {'status': receipt['status']}
 
